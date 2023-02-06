8db75d809c6c201f8e289ccde72ca9d9fdfde9f0
mahadi160 <m.mahadi228@gmail.com>
2023-02-07 01:48:14 +0600

    first commit

--- assignment.php
+++ assignment.php
@@ -1,12 +1,10 @@
 <?php
 
-$tuitionFee = 20000;
+$x = 20000;
 
-$twentyFivePercent = $tuitionFee * 25 / 100;
-$twentyPercent = $tuitionFee * 20 / 100;
-$fifteenPercent = $tuitionFee * 15 / 100;
+$twentyFivePercent = $x * 25 /100;
+$twentyPercent = $x * 20 /100;
+$fifteenPercent = $x * 15 /100;
 
-$output = ($tuitionFee >= 20000)? "$twentyFivePercent" : ($tuitionFee >= 10000 && $tuitionFee <= 20000)? "$twentyPercent" :($tuitionFee >= 7000 && $tuitionFee <= 10000)? "$fifteenPercent" : "the data will be invalid.";
-echo $output;
-
-
+$calculator = ($x >= 20000) ? "$twentyFivePercent" : (($x >= 10000 && $x <20000)? "$twentyPercent":(($x >= 7000 && $x < 10000)? "$fifteenPercent" : "the data will be invalid")) ;
+echo $calculator;
